8c41c9f5077551754ae73bfe4f2d0695928e0e3b
Confidence Eze <Confidostic3@gmail.com>
2025-01-14 21:12:03 +0100

    Script update

--- crawler.sh
+++ crawler.sh
@@ -2,18 +2,33 @@
 
 # BASH scripting 101 class
 
-# Replace posts with comments, users etc 
-CRAWL_URL="https://jsonplaceholder.typicode.com/posts"
+# Replace posts with comments, users etc.
+CRAWL_URL="https://jsonplaceholder.typicode.com/posts/2"
 SAVE_TO="content.csv"
 
-# Fetching html content and saving to temp json file - from jq plugin
-curl -s "$URL" > content.json
+# Fetching JSON content and saving to a temporary file
+curl -s "$CRAWL_URL" -o content.json
 
-# Extracting json data from content.json and write to csv file - with @csv from jq plugin
-echo "Id, Title, Body" > "$SAVE_TO" 
-jq -r '.[] | [.id, .title, .body] | @csv' content.json >> "$SAVE_TO"
+# Display the fetched JSON content (for debugging)
+# cat content.json
 
-# Remove temp json file
-rm content.json
+# Extracting JSON data and writing to a CSV file
+echo "Id,Title,Body" > "$SAVE_TO"
 
-echo "Data saved to $SAVE_TO" 
+# Processing the json data with jq plugin
+# jq -r '.[] | [.id, .title, .body] | @csv <-close single quote content.json >> $SAVE_TO
+
+# Processing the JSON data (without jq)
+grep -E '"id":|"title":|"body":' content.json | \
+sed -E 's/^[[:space:]]*"id": ([0-9]+),?/\1/' | \
+sed -E 's/^[[:space:]]*"title": "(.*)",?/\1/' | \
+sed -E 's/^[[:space:]]*"body": "(.*)"/\1/' | \
+awk 'ORS=(NR%3?",":"\n")' >> "$SAVE_TO"
+
+# Remove the temporary JSON file
+rm -f content.json
+
+# Display the saved CSV content
+# cat $SAVE_T"
+echo "Data saved to $SAVE_TO"
+
